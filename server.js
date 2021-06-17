1785024238ec445cafd76c31a202801c5b257513
Daniel Phan <dannyphan@comcast.net>
2021-06-16 23:27:36 -0700

    Cleaned up code in multiple files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,17 +7,17 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Connects to MongoDB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(require('./routes/api.js'));
+app.use(require('./routes/html.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
